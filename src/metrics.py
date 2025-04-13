d2f25622bcd668481f0ce46dfd4bc8c7d0a87f15
jeagrego <jean-nicolas.gregoire@ulb.be>
2025-04-13 17:39:30 +0200

    Adding scanpy_modified

--- src/metrics.py
+++ src/metrics.py
@@ -1,7 +1,7 @@
 import sys
 import random
 from anndata import AnnData
-import scanpy
+import scanpy_modified as scanpy
 from ripser import Rips
 import umap
 import pandas as pd
@@ -20,7 +20,7 @@ from scipy.spatial import distance
 
 
 
-def metrics_distribution_of_pairwise_distances(df, num_bins: int, visualize: bool =False) -> np.number | np.ndarray:
+def metrics_distribution_of_pairwise_distances(df, num_bins: int, visualize: bool = True) -> np.number | np.ndarray:
     """
     ref: https://doi.org/10.1371/journal.pcbi.1011866
     pg: 5
@@ -34,20 +34,23 @@ def metrics_distribution_of_pairwise_distances(df, num_bins: int, visualize: boo
     scanpy.tl.diffmap(df)
     scanpy.tl.dpt(df)
     tmp = np.stack(df.obs['dpt_distances'])
-    tmp[tmp == inf] = 1.5 * np.max(tmp[tmp != inf]) 
-    tmp[tmp == -inf] = -1 * np.min(tmp[tmp != -inf]) 
+    tmp[tmp == np.inf] = 1.5 * np.max(tmp[tmp != np.inf]) 
+    tmp[tmp == -np.inf] = -1 * np.min(tmp[tmp != -np.inf]) 
     a = plt.hist(tmp[np.triu(tmp, 1) != 0], bins = num_bins)
     hs = a[0]/np.sum(a[0])
-  
+   
     ent = entropy(hs, base=num_bins)
     
-    
+    if visualize == True:
+        plt.show()
+    elif visualize == False:
+        plt.close()
     return  ent
 
 if __name__ == "__main__":
     # Example usage
     from utils import preprocessing
-    df = pd.read_csv("/home/linsfa/Documents/BIO-INFO-2025/src/data/fig6_fig7/bone-marrow-mesenchyme-erythrocyte-differentiation_mca.rds.csv", index_col=0)
+    df = pd.read_csv("/home/jmjn/bioinformatics/project/BIO-INFO-2025/src/data/fig6_fig7/bone-marrow-mesenchyme-erythrocyte-differentiation_mca.rds.csv", index_col=0)
     df = preprocessing(df, 0.1, 0.9)
     metrics_distribution_of_pairwise_distances(df, num_bins=10)
     
