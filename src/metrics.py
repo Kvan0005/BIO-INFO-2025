a2388982b4f9fc5ea52ec13e20f9441bd6d54b3f
LinSFa <kevin.vandervaeren@ulb.be>
2025-04-20 19:06:39 +0200

    add metric 2

--- src/metrics.py
+++ src/metrics.py
@@ -17,8 +17,8 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sknetwork.clustering import Louvain
 from scipy.spatial import distance
-
-
+from math import floor
+FEATURES_MAX_THRESHOLD = 5 #? 5 is given in the paper dc about logic
 
 def metric_distribution_of_pairwise_distances(df, num_bins: int, visualize: bool = False) -> np.number | np.ndarray:
     """
@@ -51,11 +51,7 @@ def metric_persistent_homology(df, num_bins: int, visualize: bool = False) -> np
     ref: https://doi.org/10.1371/journal.pcbi.1011866
     pg: 5
     title: Scoring metric 2 -persistent homology
-    """
-    print(df.shape)
-    # get name of columns that contain NaN values
-    print(df.columns[df.isna().any()].tolist())
-    df = df.dropna(axis=1) # drop columns with NaN values  
+    """  
     df = anndata.AnnData(df)
     df.uns['iroot'] = 0
     scanpy.pp.neighbors(df)
@@ -64,10 +60,27 @@ def metric_persistent_homology(df, num_bins: int, visualize: bool = False) -> np
     tmp = np.stack(df.obs['dpt_distances'])  # type: ignore dont know how to fix this warning
     # get line with inf values
     tmp[tmp == np.inf] = np.random.normal(1.5, 0.1) * np.max(tmp[tmp != np.inf])
+    tmp[tmp == -np.inf] = -1 * np.min(tmp[tmp != -np.inf])
+    rips = Rips(maxdim=0, verbose=False)  # Initialize a Rips object to perform topological analysis of distances. maxdim=0 means we are only interested in connected components in the data (i.e., groups of linked cells).
+    crs_tmp = sparse.csr_matrix(tmp) # mapping the data matrix to a (position, distance) matrix
+    diagrams= rips.fit_transform(crs_tmp, distance_matrix=True)
+    if diagrams is None:
+        exit() # for code sanity
+    histogram = plt.hist(diagrams[0][:-1,1], bins=num_bins)
+    normalize_histogram = histogram[0]/np.sum(histogram[0])
+    entropy_value = entropy(normalize_histogram, base=num_bins)
+    entropy_value = np.log(entropy_value)
+    plt.show() if visualize else plt.close()
+    return entropy_value
+
+def metric_vector(df, cells_per_cluster:int, metric: str) -> float:
+    df = PCA(n_components=FEATURES_MAX_THRESHOLD).fit_transform(df)
+    _number_of_clusters = min(floor(len(df)/cells_per_cluster), 100)
+    
+    
 if __name__ == "__main__":
     # Example usage
     from utils import preprocessing
-    import os
     # path = "/home/linsfa/Documents/BIO-INFO-2025/src/data/fig6_fig7/"
     # for file in os.listdir(path):
     #     if file.endswith(".rds.csv"):
@@ -78,5 +91,5 @@ if __name__ == "__main__":
     path = "/home/linsfa/Documents/BIO-INFO-2025/src/data/fig6_fig7/NKT-differentiation_engel.rds.csv"
     df = pd.read_csv(path, index_col=0)
     df = preprocessing(df, 0.1, 0.9)
-    metric_persistent_homology(df, num_bins=10)
+    print(metric_persistent_homology(df, num_bins=10))
     
