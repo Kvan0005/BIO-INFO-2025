af2b6331c0560f7c193ea47d897a2f6416d0ff04
LinSFa <kevin.vandervaeren@ulb.be>
2025-05-21 13:00:48 +0200

    data_generator WIP

---
+++ src/data_generator.py
@@ -0,0 +1,82 @@
+import numpy as np
+import random
+from matplotlib.image import BboxImage
+from matplotlib.transforms import Bbox, TransformedBbox
+import sklearn
+import sklearn.metrics
+
+def make_blob(num, mu_x, mu_y, sigma):
+    #mu_, sigma = 0, 0.1 # mean and standard deviation
+    X1 = np.random.normal(mu_x, sigma, num)
+    Y1 = np.random.normal(mu_y, sigma, num)
+    Blob = np.array([X1, Y1]).T
+    return Blob
+
+def make_blob_elipse(num, mu_x, mu_y, sigma_x, sigma_y):
+    #mu_, sigma = 0, 0.1 # mean and standard deviation
+    X1 = np.random.normal(mu_x, sigma_x, num)
+    Y1 = np.random.normal(mu_y, sigma_y, num)
+    Blob = np.array([X1, Y1]).T
+    #plt.scatter(X1,Y1)
+    return Blob
+
+def gen_cluster(num=1000,seed=1):
+    np.random.seed(seed)
+    random.seed(seed)
+    num_clusters = np.floor(np.random.exponential(scale=1.5)+2)
+    prop = np.random.normal(5, 0.1, num_clusters) #SIGMA DETERMINES HOW UNBALANCED CLUSTERS ARE
+    prop = prop/np.sum(prop)
+    blobs = []
+    center_cand_x = [i for i in range(num_clusters)]
+    center_cand_y = [i for i in range(num_clusters)]
+    for i in range(num_clusters):
+        x= center_cand_x.pop(random.randrange(len(center_cand_x)))
+        y= center_cand_y.pop(random.randrange(len(center_cand_y)))
+        #print(x,y)
+        blob1 = make_blob(int(num*prop[i]),x,y,np.random.normal(0.2,0.01))
+        blobs.append(blob1)
+    C = np.concatenate(blobs)
+    return C
+
+
+def gen_cluster_random(num=1000,seed= 1):
+    np.random.seed(seed)
+    random.seed(seed)
+    num_clusters = np.random.randint(2,7)
+    prop = np.random.uniform(0, 2, num_clusters) #SIGMA DETERMINES HOW UNBALANCED CLUSTERS ARE
+    prop = prop/np.sum(prop)
+    blobs = []
+    center_cand_x = [i for i in range(num_clusters)]
+    center_cand_y = [i for i in range(num_clusters)]
+    for i in range(num_clusters):
+        x= center_cand_x.pop(random.randrange(len(center_cand_x)))
+        y= center_cand_y.pop(random.randrange(len(center_cand_y)))
+        blob1 = make_blob_elipse(int(num*prop[i]),x,y,np.random.normal(0.3,0.02),np.random.normal(0.3,0.02))
+        blobs.append(blob1)
+    C = np.concatenate(blobs)
+    return C
+
+
+def gen_cluster_random_extreme(num=1000):
+    num_clusters = np.random.randint(2,7)
+    prop = np.random.uniform(0, 2, num_clusters) #SIGMA DETERMINES HOW UNBALANCED CLUSTERS ARE
+    prop = prop/np.sum(prop)
+    blobs = []
+    center_cand_x = [i for i in range(num_clusters)]
+    center_cand_y = [i for i in range(num_clusters)]
+    xy = []
+    for _ in range(num_clusters):
+        x= center_cand_x.pop(random.randrange(len(center_cand_x)))
+        y= center_cand_y.pop(random.randrange(len(center_cand_y)))
+        xy.append([x,y])
+    
+    width = np.sqrt(np.max(sklearn.metrics.pairwise_distances(np.array(xy))))
+    for i in range(len(xy)):
+        x, y = xy[i]
+        blob1 = make_blob_elipse(int(num*prop[i]),x,y,np.random.normal(width,width/10),np.random.normal(width,width/10))
+        blobs.append(blob1)
+        
+    C = np.concatenate(blobs)
+    return C
+
+def gen_trajectory #todo
